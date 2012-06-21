18003d29fd4b0aea5783e954214551e22dcf54e1
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2012-06-21 17:50:14 +0200

    cleaning more generated files

--- vmod/clean.sh
+++ vmod/clean.sh
@@ -4,12 +4,21 @@
 # A clean script that removes all generated files and folders
 #
 
-# folders
+# src folder
+rm -r src/.deps/
+rm -r src/.libs/
+rm src/Makefile
+rm src/Makefile.in
+rm src/vcc_if.c
+rm src/vcc_if.h
+rm src/vcc_if.lo
+
 rm -r autom4te.cache/
 rm -r m4/
 
 # files
 rm *.m4
+rm clean
 rm config.*
 rm configure
 rm depcomp
